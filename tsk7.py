57728487a24797593a4f429eca2d34bc2a247f5b
leaty-ell <lisalimanova14@gmail.com>
2026-04-08 23:33:40 +0700

    Update tsk7.py

--- tsk7.py
+++ tsk7.py
@@ -11,17 +11,17 @@ class TrafficLight:
     """
     permissible_values = ['зеленый', 'желтый', 'красный', 'желтый']
     
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize a new TrafficLight object."""
         self.current_signal = self.permissible_values[0]
         self.index = 0
     
-    def next_signal(self):
+    def next_signal(self) -> None:
         """Change the traffic light to the next signal."""
         self.index = (self.index + 1) % len(self.permissible_values)
         self.current_signal = self.permissible_values[self.index]
     
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Return a string representation of the traffic light.
       
@@ -30,7 +30,7 @@ class TrafficLight:
         """
         return self.current_signal
 
-def main():
+def main() -> None:
     """The main function of the program."""
     seven_roads = TrafficLight()
     print(seven_roads.current_signal)
